3d6c3850f030e3c4f421e44c7aab8e7b7aa85186
Vanessa Vun <vanessavun@gmail.com>
2023-03-26 12:34:58 -0700

    Edited Vite config for dependency issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,12 @@ import BigBangStarField from "react-big-bang-star-field";
 function Hero() {
   return (
     <section className="flex justify-center items-center min-h-screen">
-      {/* <BigBangStarField numStars={200} maxStarSpeed={1} scale={2} className='absolute w-full h-full' starColor={"217, 160, 244"} /> */}
+      <BigBangStarField numStars={200} maxStarSpeed={1} scale={2} className='absolute w-full h-full' starColor={"217, 160, 244"} />
       <div className="container flex">
         <div className="hero-content md:flex justify-center">
           <div className="flex flex-col justify-center p-4">
             <Fade left>
-              <h2 className="text- text-indigo-200 font-semibold font-sans sm:text-lg">
+              <h2 className=" text-indigo-200 font-semibold font-sans sm:text-lg">
                 👋 Hi, my name is
               </h2>
               <h1 className="text-5xl font-bold text-purple-300 pt-4 font-sacramento">
